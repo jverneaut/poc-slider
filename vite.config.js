0a2cd0888b091a858c9f2a651bf21c4700ca8028
Julien Verneaut <jverneaut@gmail.com>
2022-04-26 15:35:02 +0200

    Output multiple html files

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,21 @@ import { defineConfig } from 'vite';
 import twig from 'vite-plugin-twig';
 
 const root = resolve(__dirname, 'src');
+const outDir = resolve(__dirname, 'dist');
 
 export default defineConfig({
   plugins: [twig()],
   root,
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        index: resolve(root, 'index.html'),
+        rosheim: resolve(root, 'rosheim.html'),
+        paris: resolve(root, 'paris.html'),
+        strasbourg: resolve(root, 'strasbourg.html'),
+      },
+    },
+  },
 });
